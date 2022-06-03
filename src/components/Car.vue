fa61b32230ccecbe529656c176c3e535bd522aaa
danielg2017 <danieledgardo826@gmail.com>
2022-06-03 12:57:26 -0300

    configuracion

--- src/components/Car.vue
+++ src/components/Car.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
     <h2>Coche - Composition API</h2>
     <p>Marca: {{ brand }}</p>
     <p>Modelo: {{ model }}</p>
@@ -8,42 +9,43 @@
     <!-- proceso para pasar la funcion de forma directa -->
     <button @click="upPower">Aumentar potencia</button>
     <button @click="downPower">Disminuir Potencia</button>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        power: {
-            type: Number,
-            default: 280,
-        },
-        upPower: {
-            type: Function,
-            default: () => {},
-        },
-        downPower: {
-            type: Function,
-            default: () => {},
-        },
+  props: {
+    power: {
+      type: Number,
+      default: 280,
     },
-
-    setup(props) {
-        const brand = "Ford";
-        const model = "Mustang";
-
-        // crear un funcion y exportarla para utilizar en el mismo componente
-        // console.log(props);
-
-        // const testPower = () => {
-        //     props.upPower();
-        // };
-
-        return {
-            brand,
-            model,
-            // testPower,
-        };
+    upPower: {
+      type: Function,
+      default: () => {},
     },
+    downPower: {
+      type: Function,
+      default: () => {},
+    },
+  },
+
+  setup(props) {
+    const brand = "Ford";
+    const model = "Mustang";
+
+    // crear un funcion y exportarla para utilizar en el mismo componente
+    // console.log(props);
+
+    // const testPower = () => {
+    //     props.upPower();
+    // };
+
+    return {
+      brand,
+      model,
+      // testPower,
+    };
+  },
 };
 </script>
 
